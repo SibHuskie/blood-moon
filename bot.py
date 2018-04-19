7a788d479f5a4afb96e049b47720eb4ef0066f62
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-04-20 09:16:15 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,3 +17,48 @@ async def on_ready():
     print("---------------")
     await client.change_presence(game=discord.Game(name='with Huskie'))
 client.run(os.environ['BOT_TOKEN'])
+
+# %tempmute <user> <time> [reason]
+@client.command(pass_context=True)
+async def tempmute(ctx, userName: discord.Member = None, time: int = None, *, args = None):
+    punished_role = discord.utils.get(ctx.message.server.roles, name='Muted')
+    helper_role = discord.utils.get(ctx.message.server.roles, name='CHAT MODS')
+    mod_role = discord.utils.get(ctx.message.server.roles, name='MOD')
+    admin_role = discord.utils.get(ctx.message.server.roles, name='Administrator')
+    dev_role = discord.utils.get(ctx.message.server.roles, name ='DEVELOPER')
+    manager_role = discord.utils.get(ctx.message.server.roles, name='CO-FOUNDERS')
+    owner_role = discord.utils.get(ctx.message.server.roles, name='FOUNDERS')
+    author = ctx.message.author
+    msg = discord.Embed(colour=0x9b0019, description= "")
+    msg.title = ""
+    msg.set_footer(text=footer_text)
+    if helper_role in author.roles or mod_role in author.roles or admin_role in author.roles or dev_role in author_roles or manager_role in author.roles or owner_role in author.roles:
+        if userName == None or time == None:
+            msg.add_field(name=":new_moon: ", value="`%tempmute <user> <time> [reason]`")
+            await client.say(embed=msg)
+        elif helper_role in userName.roles or mod_role in userName.roles or admin_role in userName.roles or manager_role in userName.roles or owner_role in userName.roles:
+            msg.add_field(name=":warning: ", value="`You can't punish other staff!`")
+            await client.say(embed=msg)
+        elif punished_role in userName.roles:
+            msg.add_field(name=":warning: ", value="`That user is already punished!`")
+            await client.say(embed=msg)
+        else:
+            time2 = time * 60
+            if args == None:
+                await client.add_roles(userName, punished_role)
+                await client.remove_roles(userName, member_role)
+                msg.add_field(name=":speak_no_evil: ", value="`{} has been punished by {}! for {} minute(s)!`\n`Reason: ?`".format(userName.display_name, author.display_name, time))
+                await client.say(embed=msg)
+                await asyncio.sleep(float(time2))
+                await client.remove_roles(userName, punished_role)
+                await client.say("```diff\n- Removed {}'s punishment! ({} minute(s) are up.)\n```".format(userName.display_name, time))
+            else:
+                await client.add_roles(userName, punished_role)
+                msg.add_field(name=":speak_no_evil: ", value="`{} has been punished by {} for {} minute(s)!`\n`Reason: {}`".format(userName.display_name, author.display_name, time, args))
+                await client.say(embed=msg)
+                await asyncio.sleep(float(time2))
+                await client.remove_roles(userName, punished_role)
+                await client.say("```diff\n- Removed {}'s punishment! ({} minute(s) are up.)\n```".format(userName.display_name, time))
+    else:
+        msg.add_field(name=":warning: ", value="`This command can only be used by staff!`")
+        await client.say(embed=msg)
