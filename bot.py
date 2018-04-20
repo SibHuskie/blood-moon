d8769ab5f048b3e85a17ec4dd4609f3a25999f8e
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-04-20 10:27:46 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("---------------")
-    await client.change_presence(game=discord.Game(name='with Huskie'))
+    await client.change_presence(game=discord.Game(name='with Earth'))
 
 # %tempmute <user> <time> [reason]
 @client.command(pass_context=True)
