0727c31c419c0bf626145b120839b06e77d804e1
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-04-20 10:40:15 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -140,4 +140,23 @@ async def unban(ctx, userID = None):
     else:
         msg.add_field(name=":warning: ", value="`This command can only be used by Moderators, Administrators, Co Founders and Founders!`")
     await client.say(embed=msg)
+    
+# %say <text>
+@client.command(pass_context=True)
+async def echo(ctx, *, args=None): 
+    staff_role = discord.utils.get(ctx.message.server.roles, name='Staff')
+    author = ctx.message.author
+    msg = discord.Embed(colour=0x9b0019, description= "")
+    msg.title = ""
+    msg.set_footer(text=footer_text)
+    if staff_role in author.roles or staff_role in author.roles:
+        if args == None:
+            msg.add_field(name=":warning: ", value="%say <text>")
+            await client.say(embed=msg)
+        else:
+            await client.say("{}".format(args))
+            await client.delete_message(ctx.message)
+    else:
+        msg.add_field(name=":warning: ", value="`This command can only be used by Staff!`")
+        await client.say(embed=msg)
 client.run(os.environ['BOT_TOKEN'])
