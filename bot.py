a0a568a4582e84b7303fea76a8b3d4fa60c5274e
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-04-20 10:36:25 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,4 +114,30 @@ async def ban(ctx, userName: discord.Member = None, *, args = None):
     else:
         msg.add_field(name=":warning: ", value="`This command can only be used by Moderators, Admins, Co Founders and Founders!`")
     await client.say(embed=msg)
+
+# %unban <user id>
+@client.command(pass_context=True)
+async def unban(ctx, userID = None):
+    mod_role = discord.utils.get(ctx.message.server.roles, name='MOD')
+    admin_role = discord.utils.get(ctx.message.server.roles, name='ADMIN')
+    manager_role = discord.utils.get(ctx.message.server.roles, name='CO-FOUNDERS')
+    owner_role = discord.utils.get(ctx.message.server.roles, name='FOUNDERS')
+    author = ctx.message.author
+    msg = discord.Embed(colour=0x9b0019, description= "")
+    msg.title = ""
+    msg.set_footer(text=footer_text)
+    if mod_role in author.roles or admin_role in author.roles or manager_role in author.roles or owner_role in author.roles:
+        if userID == None:
+            msg.add_field(name=":warning: ", value="`%unban <user id>`")
+        else:
+            banned_users = await client.get_bans(ctx.message.server)
+            user = discord.utils.get(banned_users,id=userID)
+            if user is not None:
+                await client.unban(ctx.message.server, user)
+                msg.add_field(name=":tools: ", value="`{} unbanned the user with the following ID: {}!`".format(author.display_name, userID))
+            else:
+                msg.add_field(name=":octagonal_sign: ", value="`The ID you specified is not banned! ID: {}`".format(userID))
+    else:
+        msg.add_field(name=":warning: ", value="`This command can only be used by Moderators, Administrators, Co Founders and Founders!`")
+    await client.say(embed=msg)
 client.run(os.environ['BOT_TOKEN'])
